4dc642919638e8134136bdd91ee1167235a7661e
Sanjit <ssanjit856@gmail.com>
2022-04-19 23:58:48 +0530

    removed homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,7 @@
-import { Route, Routes } from "react-router-dom";
-import HomePage from "./pages/HomePage";
+import React from "react";
 import "./App.css";
 function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-    </Routes>
-  );
+  return <></>;
 }
 
 export default App;
