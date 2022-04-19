1a9dde7e6d250b50a567566b146ffb37c6afb140
Sanjit <ssanjit856@gmail.com>
2022-04-20 02:05:36 +0530

    removed password from auth response

--- src/api/controllers/authController.js
+++ src/api/controllers/authController.js
@@ -3,11 +3,19 @@ const loginController = async (req, res) => {
   const { email, password } = req.body;
   try {
     const user = await User.login(email, password);
+
     if (user) {
-      res.json(user);
+      res.json({
+        _id: user._id,
+        profilePictureURL: user.profilePictureURL,
+        email: user.email,
+        name: user.name,
+        updatedAt: user.updatedAt,
+        createdAt: user.createdAt,
+        token: user.token,
+      });
     }
   } catch (err) {
-    console.log(err);
     res.status(401).json({ errors: [err.message.split(",")] });
   }
 };
@@ -18,8 +26,10 @@ const signupController = async (req, res) => {
     res.json({
       _id: user._id,
       email: user.email,
+      name: user.name,
+      profilePictureURL: user.profilePictureURL,
       updatedAt: user.updatedAt,
-      updatedAt: user.updatedAt,
+      createdAt: user.createdAt,
       token: user.token,
     });
   } catch (err) {
