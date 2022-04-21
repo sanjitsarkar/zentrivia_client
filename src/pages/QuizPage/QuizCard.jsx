1bd75d5a2fb788eef1e3f2d29da5e99e0f3c9e1d
Sanjit <ssanjit856@gmail.com>
2022-04-21 15:45:50 +0530

    changed quiz card bg color

--- src/pages/QuizPage/QuizCard.jsx
+++ src/pages/QuizPage/QuizCard.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const QuizCard = ({ quiz }) => {
   return (
-    <div className="quiz-card cursor-pointer row  gap-1  text-dark-2 p-2 br-sm">
+    <div className="quiz-card cursor-pointer row  gap-1  text-light p-2 br-sm">
       <img
         className="w-40 br-sm h-full"
         src={quiz.quizCoverImage}
@@ -11,7 +11,7 @@ const QuizCard = ({ quiz }) => {
       <div className="col  gap-1 h-full  quiz-card-bottom justify-center items-center">
         <h1 className="text-xl font-medium">{quiz.title}</h1>
         <div className="row gap-2 justify-center items-center">
-          <button className="quiz-play-button p-1 pl-3 text-light text-md pr-3 bg-dark">
+          <button className="quiz-play-button  pl-3 text-dark text-md pr-3 ">
             Play
           </button>
           <div className="row quiz-bottom-bar justify-center">
@@ -24,7 +24,7 @@ const QuizCard = ({ quiz }) => {
               {quiz.totalPlayedUser.length > 1 && "s"} played
             </span>
             <span
-              className={`p-05 pl-2 br-sm pr-2 text-sm text-light ${
+              className={`p-05  pl-2 br-sm pr-2 text-sm text-light ${
                 quiz.quizDifficulty === "Hard" ? "bg-tertiary" : ""
               } ${quiz.quizDifficulty === "Medium" ? "bg-secondary" : ""} ${
                 quiz.quizDifficulty === "Easy" ? "bg-primary" : ""
