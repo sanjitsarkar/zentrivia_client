2a6b565a7dd88d9565cb7d688bba81e8346f7ca8
Sanjit <ssanjit856@gmail.com>
2022-04-28 20:13:52 +0530

    fixed client header problem

--- src/api/middlewares/authenticateToken.js
+++ src/api/middlewares/authenticateToken.js
@@ -7,7 +7,7 @@ const authenticateToken = (req, res, next) => {
   if (!token) return res.sendStatus(401);
   else {
     jwt.verify(token, process.env.JWT_TOKEN_KEY, (err, user) => {
-      if (err) return res.sendStatus(404).send("Forbidden");
+      if (err) return res.sendStatus(404);
       else {
         req.user = user;
 
